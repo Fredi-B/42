afe19a05e51a21568c44f2db7e8bc66cd759a1ae
Fredi-B <fredibechtoldt@outlook.com>
2022-01-13 19:55:08 +0100

    wrote get_cmds(). Seems to work, not protected yet. Work in progress

--- pipex/mandatory/parsing.c
+++ pipex/mandatory/parsing.c
@@ -1,16 +1,12 @@
 #include "pipex.h"
 static char	*get_paths_from_env(char **env, t_pipex *data);
 static int	get_prefix_len(char *env);
-
+static void	get_cmds(int argc, char **argv, t_pipex *data);
 
 void	put_input_in_struct(int argc, char **argv, char **env, t_pipex *data)
 {
 	get_paths_from_env(env, data);
-	dsprintf(data->paths[1]);
-	
-	/* to use unused parameters... */
-	argc = 1;
-	argv[2] = "test";
+	get_cmds(argc, argv, data);
 }
 
 static char	*get_paths_from_env(char **env, t_pipex *data)
@@ -53,7 +49,48 @@ static int	get_prefix_len(char *env)
 	{
 		free(prefix[i]);
 		i++;
-	} 
+	}
 	free(prefix);
 	return (len + 1);
+}
+
+static void	get_cmds(int argc, char **argv, t_pipex *data)
+{
+	t_cmd_node	*new_node;
+	int			i;
+	char		**tmp_argv;
+
+	i = argc - 2;
+	while (i >= 2)
+	{
+		new_node = (t_cmd_node *)malloc(sizeof(t_cmd_node));
+		/* protecten mit exit fct, in der alles gefreet wird unso (s. push_swap) */
+		if (!new_node)
+			return ;
+		data->cmds = new_node;
+		data->cmds->path_and_flags = argv[i];
+		/* to do: free tmp_argv */
+		tmp_argv = ft_split(argv[i], ' ');
+		// if (!tmp_argv)
+		// 	err_exit(data, "", 0);
+		data->cmds->cmd_path = tmp_argv[0];
+		if (data->cmds == NULL)
+		{
+			data->head = new_node;
+			data->head->next = NULL;
+		}
+		else
+		{
+			new_node->next = data->head;
+			data->head = new_node;
+		}
+		i--;
+	}
+	/* for checking only */
+	while (data->cmds)
+	{
+		dsprintf(data->cmds->path_and_flags);
+		dsprintf(data->cmds->cmd_path);
+		data->cmds = data->cmds->next;
+	}
 }
