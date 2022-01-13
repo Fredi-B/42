0450ac13612cc8e4ead7cc937321ebd17864fcda
Fredi-B <fredibechtoldt@outlook.com>
2022-01-13 20:00:50 +0100

    added linked_list_struct for the cmds. Added prototypes for err_exit.c. Small debugging define changes

--- pipex/mandatory/pipex.h
+++ pipex/mandatory/pipex.h
@@ -6,7 +6,7 @@
 /*   By: fbechtol <fbechtol@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 15:11:58 by fbechtol          #+#    #+#             */
-/*   Updated: 2022/01/12 17:18:46 by fbechtol         ###   ########.fr       */
+/*   Updated: 2022/01/13 19:27:36 by fbechtol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,19 +29,29 @@
 
 /* ---------------------- Defines for debugging ---------------------------- */
 
-# define dsprintf(expr) printf("\n" #expr ":\n%s\n", expr)
-# define diprintf(expr) printf("\n" #expr ":\n%d\n", expr)
+# define dsprintf(expr) printf("\n" #expr ":\n|%s|\n", expr)
+# define diprintf(expr) printf("\n" #expr ":\n|%d|\n", expr)
 
 
 /* ----------------------------- Structures -------------------------------- */
+
+typedef struct s_cmd_node
+{
+	/* here goes cmd joined with path */
+	char				*cmd_path;
+	/* here goes cmd joined with path and flags */
+	char				*path_and_flags;
+	struct s_cmd_node	*next;
+}				t_cmd_node;
 
 typedef struct s_pipex
 {
 	char	**paths;
 	char	**files;
-	/* cmds als linked list? in eigenem struct */
-	char	**cmds;
 	char	**envp;
+	/* brauch i das? oder übergebe ich beide struct seperat lieber? */
+	t_cmd_node	*cmds;
+	t_cmd_node	*head;
 }				t_pipex;
 
 /*  ------------------------ Function prototypes --------------------------- */
@@ -49,5 +59,10 @@ typedef struct s_pipex
 /*  ----------------------------- parsing.c -------------------------------- */
 
 void	put_input_in_struct(int argc, char **argv, char **env, t_pipex *data);
+
+/*  ----------------------------- err_exit.c -------------------------------- */
+
+void	err_exit(t_pipex *data, char *msg, int len);
+void	free_two_d_arr(char **arr);
 
 #endif
