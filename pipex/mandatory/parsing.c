0f9405781ce13486abe1c666e89d1307a71137e0
Fredi-B <fredibechtoldt@outlook.com>
2022-01-13 15:04:03 +0100

    added get_prefix_len() as a prep for minishell

--- pipex/mandatory/parsing.c
+++ pipex/mandatory/parsing.c
@@ -1,22 +1,22 @@
 #include "pipex.h"
 static char	*get_paths_from_env(char **env, t_pipex *data);
+static int	get_prefix_len(char *env);
+
 
 void	put_input_in_struct(int argc, char **argv, char **env, t_pipex *data)
 {
-
 	get_paths_from_env(env, data);
-	dsprintf(data->paths[2]);
-	
+	dsprintf(data->paths[1]);
 	
 	/* to use unused parameters... */
 	argc = 1;
 	argv[2] = "test";
-	write(1, "test\n", 6);
 }
 
 static char	*get_paths_from_env(char **env, t_pipex *data)
 {
 	int		i;
+	int		len_prefix;
 
 	i = 0;
 	while (1)
@@ -29,8 +29,9 @@ static char	*get_paths_from_env(char **env, t_pipex *data)
 		}
 		if (ft_strnstr(env[i], "PATH=", ft_strlen(env[i])) != NULL)
 		{
+			len_prefix = get_prefix_len(env[i]);
 			/* to do: free data->paths */
-			data->paths = ft_split(&env[i][5], ':');
+			data->paths = ft_split(&env[i][len_prefix], ':');
 			break ;
 		}
 		i++;
@@ -38,3 +39,21 @@ static char	*get_paths_from_env(char **env, t_pipex *data)
 	return (NULL);
 }
 
+static int	get_prefix_len(char *env)
+{
+	char	**prefix;
+	int		len;
+	int		i;
+
+	len = 0;
+	prefix = ft_split(env, '=');
+	len = ft_strlen(prefix[0]);
+	i = 0;
+	while (prefix[i])
+	{
+		free(prefix[i]);
+		i++;
+	} 
+	free(prefix);
+	return (len + 1);
+}
