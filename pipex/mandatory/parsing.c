2ce8242a33f45ce7614c6e8ee8ef1085924592dc
Fredi-B <fredibechtoldt@outlook.com>
2022-01-19 20:04:21 +0100

    put util fct in _utils.c and vice versa. Protected malloc, deleted comments

--- pipex/mandatory/parsing.c
+++ pipex/mandatory/parsing.c
@@ -1,7 +1,9 @@
 #include "pipex.h"
+
 static char	*get_paths_from_env(char **env, t_pipex *data);
-static int	get_prefix_len(char *env);
 static void	get_cmds(int argc, char **argv, t_pipex *data);
+static void	join_path_and_cmds(t_pipex *data);
+static void	get_files(int argc, char **argv, t_pipex *data);
 
 void	parsing(int argc, char **argv, char **env, t_pipex *data)
 {
@@ -20,15 +22,10 @@ static char	*get_paths_from_env(char **env, t_pipex *data)
 	while (1)
 	{
 		if (env[i] == NULL)
-		{
-			/* change to "command not found: 'cmd'" (s. Alf)" */
-			write(STDERR_FILENO, "PATH not found\n", 15);
-			break ;
-		}
-		if (ft_strnstr(env[i], "PATH=", ft_strlen(env[i])) != NULL)
+			err_exit(data, "PATH not found\n", 15);
+		if (ft_strnstr(env[i], "PATH=", ft_strlen("PATH=")) != NULL)
 		{
 			len_prefix = get_prefix_len(env[i]);
-			/* to do: free data->paths */
 			data->paths = ft_split(&env[i][len_prefix], ':');
 			break ;
 		}
@@ -37,39 +34,18 @@ static char	*get_paths_from_env(char **env, t_pipex *data)
 	return (NULL);
 }
 
-static int	get_prefix_len(char *env)
-{
-	char	**prefix;
-	int		len;
-	int		i;
-
-	len = 0;
-	prefix = ft_split(env, '=');
-	len = ft_strlen(prefix[0]);
-	i = 0;
-	while (prefix[i])
-	{
-		free(prefix[i]);
-		i++;
-	}
-	free(prefix);
-	return (len + 1);
-}
-
 static void	get_cmds(int argc, char **argv, t_pipex *data)
 {
 	t_cmd_node	*new_node;
 	int			i;
 	char		**tmp_argv;
 
-
 	i = argc - 2;
 	while (i >= 2)
 	{
 		new_node = (t_cmd_node *)malloc(sizeof(t_cmd_node));
-		/* protecten mit exit fct, in der alles gefreet wird unso (s. push_swap) */
 		if (!new_node)
-			return ;
+			err_exit(data, "Error: malloc new_node\n", 23);
 		data->cmds = new_node;
 		/* Notiz an mich: hab path_and_flags von * zu ** gewechselt.
 		Unschöne Nebenwirkung, dass ich jetzt argv[i]
@@ -77,9 +53,11 @@ static void	get_cmds(int argc, char **argv, t_pipex *data)
 		data->cmds->path_and_flags = &argv[i];
 		/* to do: free tmp_argv */
 		tmp_argv = ft_split(argv[i], ' ');
-		// if (!tmp_argv)
-		// 	err_exit(data, "", 0);
+		if (!tmp_argv)
+			err_exit(data, "Error: split argv[i]", 20);
 		data->cmds->cmd_path = ft_strdup(tmp_argv[0]);
+		if (!data->cmds->cmd_path)
+			err_exit(data, "Error: strdup(tmp_argv[0]", 25);
 		free_two_d_arr(tmp_argv);
 		if (data->cmds == NULL)
 		{
@@ -93,4 +71,61 @@ static void	get_cmds(int argc, char **argv, t_pipex *data)
 		}
 		i--;
 	}
+}
+
+static void	join_path_and_cmds(t_pipex *data)
+{
+	char	*tmp_c_p;
+	char	*tmp_p_f;
+	int		i;
+
+	data->cmds = data->head;
+	while (data->cmds != NULL)
+	{
+		i = 0;
+		while (data->paths[i] != NULL)
+		{
+			tmp_c_p = add_slash_and_join(data->paths[i], data->cmds->cmd_path);
+			if (access(tmp_c_p, X_OK) == 0)
+			{
+				free(data->cmds->cmd_path);
+				/* to do: free data->cmds->cmd_path */
+				data->cmds->cmd_path = tmp_c_p;
+				tmp_p_f = add_slash_and_join(data->paths[i], *data->cmds->path_and_flags);
+				/* to do: free data->cmds->path_and_flags */
+				data->cmds->path_and_flags = ft_split(tmp_p_f, ' ');		
+				free(tmp_p_f);
+				break ;
+			}
+			free(tmp_c_p);
+			i++;
+			if (data->paths[i] == NULL)
+			{
+				write(2, "command not found: ", 19);
+				err_exit(data, data->cmds->cmd_path, ft_strlen(data->cmds->cmd_path));
+			}
+			/* hier schon prüfen, ob die cmds aus dem Input valide sind? 
+			d.h. prüfen, ob if (access()) in der while Schleife mal wahr war.
+			Wenn nicht, mit Fehlermsg raus? */
+		}
+		data->cmds = data->cmds->next;
+	}
+	/* for checking only */
+	// data->cmds = data->head;
+	// while (data->cmds)
+	// {
+	// 	dsprintf(data->cmds->path_and_flags);
+	// 	dsprintf(data->cmds->cmd_path);
+	// 	data->cmds = data->cmds->next;
+	// }
+
+}
+
+static void	get_files(int argc, char **argv, t_pipex *data)
+{
+	data->files = (char **)malloc(sizeof(char *) * (2 + 1));
+	/* to do: protecten */
+	data->files[0] = ft_strdup(argv[1]);
+	data->files[1] = ft_strdup(argv[argc -1]);
+	data->files[2] = NULL;
 }
