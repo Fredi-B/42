ebec2b8f3df88c3a531bc737c4d3136e27845df4
Fredi-B <fredibechtoldt@outlook.com>
2022-01-20 10:17:03 +0100

    deleted print debugs and new line char in err_exit msg

--- pipex/mandatory/pipex.c
+++ pipex/mandatory/pipex.c
@@ -8,16 +8,9 @@ int	main(int argc, char **argv, char **env)
 	t_pipex	data;
 
 	init_struct(&data, env);
-	write(1, "1\n", 2);
 	parsing(argc, argv, env, &data);
-	write(1, "2\n", 2);
-	
 	piping(&data);
-	write(1, "3\n", 2);
-	
 	free_data(&data);
-	write(1, "4\n", 2);
-
 	return (0);
 }
 
@@ -40,7 +33,7 @@ static void	cpy_env(char **env, t_pipex *data)
 		env_cnt++;
 	data->envp = (char **)malloc(sizeof(char *) * (env_cnt + 1));
 	if (!data->envp)
-		err_exit(data, "Error: cpy_env\n", 15);
+		err_exit(data, "Error: cpy_env", 14);
 	data->envp[env_cnt] = NULL;
 	env_cnt--;
 	while (env_cnt >= 0)
