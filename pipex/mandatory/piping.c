6c2ae70fd5bf98b25273e9a5a55c252a4569e0c8
Fredi-B <fredibechtoldt@outlook.com>
2022-01-20 15:40:35 +0100

    added error handling. seems to work, norm conform

--- pipex/mandatory/piping.c
+++ pipex/mandatory/piping.c
@@ -10,7 +10,7 @@ void	piping(t_pipex *data)
 		err_exit(data, "Error: malloc data->fd_pipe", 27);
 	if (pipe(data->fd_pipe) == -1)
 		err_exit(data, "Error: create pipe", 18);
-	data->cmds = data->head; //woanners hin? durchloopen?
+	data->cmds = data->head;
 	data->pid1 = fork();
 	if (data->pid1 == -1)
 		err_exit(data, "Error: create fork", 18);
@@ -19,7 +19,7 @@ void	piping(t_pipex *data)
 	if (data->pid1 != 0)
 	{
 		wait(NULL);
-		data->cmds = data->cmds->next; //woanners hin? durchloopen?
+		data->cmds = data->cmds->next;
 		data->pid2 = fork();
 		if (data->pid2 == -1)
 			err_exit(data, "Error: create fork", 18);
@@ -37,15 +37,21 @@ static void	write_in_pipe(t_pipex *data)
 
 	close(data->fd_pipe[0]);
 	if (dup2(data->fd_pipe[1], STDOUT_FILENO) == -1)
-		err_exit(data, "Error: redirect STDOUT", 19);
+		err_exit(data, "Error: redirect STDOUT", 22);
 	close(data->fd_pipe[1]);
 	fd_infile = open(data->files[0], O_CREAT | O_RDONLY, 0777);
+	if (fd_infile == -1)
+		err_exit(data, "Error: infile: permission denied", 32);
 	if (dup2(fd_infile, STDIN_FILENO) == -1)
-		err_exit(data, "Error: redirect STDIN", 18);
+		err_exit(data, "Error: redirect STDIN", 21);
 	close(fd_infile);
-	if (execve(data->cmds->cmd_path, data->cmds->path_and_flags, data->envp) == -1)
-		err_exit(data, "Error: execve cmd1", 18);
-
+	if (execve \
+		(data->cmds->cmd_path, data->cmds->path_and_flags, data->envp) == -1)
+	{
+		write(2, "command not found: ", 19);
+		write(2, data->cmds->cmd_path, ft_strlen(data->cmds->cmd_path));
+		err_exit(data, "", 0);
+	}
 }
 
 static void	read_from_pipe(t_pipex *data)
@@ -54,12 +60,19 @@ static void	read_from_pipe(t_pipex *data)
 
 	close(data->fd_pipe[1]);
 	if (dup2(data->fd_pipe[0], STDIN_FILENO) == -1)
-		err_exit(data, "Error: redirect STDIN", 18);
+		err_exit(data, "Error: redirect STDIN", 21);
 	close(data->fd_pipe[0]);
 	fd_outfile = open(data->files[1], O_CREAT | O_WRONLY | O_TRUNC, 0777);
+	if (fd_outfile == -1)
+		err_exit(data, "Error: outfile: permission denied", 33);
 	if (dup2(fd_outfile, STDOUT_FILENO) == -1)
-		err_exit(data, "Error: redirect STDOUT", 19);
+		err_exit(data, "Error: redirect STDOUT", 22);
 	close(fd_outfile);
-	if (execve(data->cmds->cmd_path, data->cmds->path_and_flags, data->envp) == -1)
-		err_exit(data, "Error: execve cmd2", 18);
+	if (execve \
+		(data->cmds->cmd_path, data->cmds->path_and_flags, data->envp) == -1)
+	{
+		write(2, "command not found: ", 19);
+		write(2, data->cmds->cmd_path, ft_strlen(data->cmds->cmd_path));
+		err_exit(data, "", 0);
+	}
 }
