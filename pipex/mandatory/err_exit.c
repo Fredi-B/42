8aaf9b38b6520a5c979b964c3aedc33849dd1948
Fredi-B <fredibechtoldt@outlook.com>
2022-01-20 10:13:33 +0100

    put free_data() call at the botton of err_exit(), so that data isn't freed before writing it. deleted debug prints

--- pipex/mandatory/err_exit.c
+++ pipex/mandatory/err_exit.c
@@ -6,21 +6,12 @@ void	free_data(t_pipex *data)
 {
 	if (data->paths != NULL)
 		free_two_d_arr(data->paths);
-	write(1, "2\n", 2);
-
 	if (data->files != NULL)
 		free_two_d_arr(data->files);
-	write(1, "3\n", 2);
-
 	if (data->envp != NULL)
 		free_two_d_arr(data->envp);
-	write(1, "4\n", 2);
-
-	dsprintf(data->cmds->cmd_path);
 	if (data->cmds != NULL)
 		free_list(data);
-	write(1, "5\n", 2);
-
 	if (data->fd_pipe != NULL)
 	{
 		free(data->fd_pipe);
@@ -32,8 +23,9 @@ void	free_data(t_pipex *data)
 Frees already malloced nodes. */
 void	err_exit(t_pipex *data, char *msg, int len)
 {
-	free_data(data);
 	write(STDERR_FILENO, msg, len);
+	write(STDERR_FILENO, "\n", 1);
+	free_data(data);
 	exit(0);
 }
 
@@ -47,8 +39,6 @@ static void	free_list(t_pipex *data)
 		while (data->cmds != NULL)
 		{
 			tmp = data->cmds->next;
-			dsprintf(data->cmds->path_and_flags[0]);
-			dsprintf(data->cmds->path_and_flags[1]);
 			if (data->cmds->path_and_flags != NULL)
 				free_two_d_arr(data->cmds->path_and_flags);
 			free(data->cmds->cmd_path);
