43c66ba3ee478b31f8eb2b46bbc1678f9ab8e880
Fredi-B <fredibechtoldt@outlook.com>
2022-01-21 19:04:11 +0100

    added fd_pipe_b to free

---
+++ pipex/bonus/err_exit_bonus.c
@@ -0,0 +1,73 @@
+#include "pipex_bonus.h"
+
+static void	free_list(t_pipex *data);
+
+void	free_data(t_pipex *data)
+{
+	if (data->paths != NULL)
+		free_two_d_arr(data->paths);
+	if (data->files != NULL)
+		free_two_d_arr(data->files);
+	if (data->envp != NULL)
+		free_two_d_arr(data->envp);
+	if (data->cmds != NULL)
+		free_list(data);
+	if (data->fd_pipe_a != NULL)
+	{
+		free(data->fd_pipe_a);
+		data->fd_pipe_a = NULL;
+	}
+	if (data->fd_pipe_b != NULL)
+	{
+		free(data->fd_pipe_b);
+		data->fd_pipe_b = NULL;
+	}
+}
+
+/* exit fct for errors during parsing / input checking.
+Frees already malloced nodes. */
+void	err_exit(t_pipex *data, char *msg, int len)
+{
+	write(STDERR_FILENO, msg, len);
+	write(STDERR_FILENO, "\n", 1);
+	free_data(data);
+	exit(0);
+}
+
+static void	free_list(t_pipex *data)
+{
+	t_cmd_node	*tmp;
+
+	data->cmds = data->head;
+	if (data->head != NULL)
+	{
+		while (data->cmds != NULL)
+		{
+			tmp = data->cmds->next;
+			if (data->cmds->path_and_flags != NULL)
+				free_two_d_arr(data->cmds->path_and_flags);
+			free(data->cmds->cmd_path);
+			data->cmds->cmd_path = NULL;
+			free(data->cmds);
+			data->cmds = NULL;
+			data->cmds = tmp;
+		}
+	}
+}
+
+void	free_two_d_arr(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i] != NULL)
+		i++;
+	while (i > 0)
+	{
+		free(arr[i - 1]);
+		arr[i - 1] = NULL;
+		i--;
+	}
+	free(arr);
+	arr = NULL;
+}
