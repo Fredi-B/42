ee2987492eb489f81c1d4b396b85020a7ac98cf2
Fredi-B <fredibechtoldt@outlook.com>
2022-01-21 19:53:02 +0100

    added piping() subfcts for norm reasons and better looks. seems to work, norm conform

--- pipex/bonus/piping_utils_bonus.c
+++ pipex/bonus/piping_utils_bonus.c
@@ -3,6 +3,48 @@
 static void	prep_pipe_b(t_pipex *data);
 static void	prep_pipe_a(t_pipex *data);
 
+int	fork_write_child(t_pipex *data)
+{
+	int	pid_w;
+
+	data->fd_pipe_a = (int *)malloc(sizeof(int) * 2);
+	if (!data->fd_pipe_a)
+		err_exit(data, "Error: malloc data->fd_pipe_a", 29);
+	if (pipe(data->fd_pipe_a) == -1)
+		err_exit(data, "Error: create pipe_a", 20);
+	data->cmds = data->head;
+	pid_w = fork();
+	if (pid_w == -1)
+		err_exit(data, "Error: create fork w", 20);
+	return (pid_w);
+}
+
+int	check_for_middle_cmds(t_pipex *data)
+{
+	int	pid_w_and_r;
+
+	wait(NULL);
+	data->pipe_flag = PIPE_B;
+	data->pipe_w = data->fd_pipe_a;
+	data->cmds = data->cmds->next;
+	while (data->cmds->next != NULL)
+	{
+		check_pipe_flag(data);
+		pid_w_and_r = fork();
+		if (pid_w_and_r == -1)
+			err_exit(data, "Error: create fork w_and_r", 26);
+		if (pid_w_and_r == 0)
+			read_write_pipe(data, data->pipe_r, data->pipe_w);
+		else
+		{
+			close(data->pipe_r[0]);
+			wait(NULL);
+		}
+		data->cmds = data->cmds->next;
+	}
+	return (pid_w_and_r);
+}
+
 void	check_pipe_flag(t_pipex *data)
 {
 	if (data->pipe_flag == PIPE_B)
