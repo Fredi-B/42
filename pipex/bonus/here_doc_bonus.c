d70a0d98feefc57bbc003b20c228abaf7423fa3d
Fredi-B <fredibechtoldt@outlook.com>
2022-01-26 15:47:10 +0100

    fixed strdup in read_from_here_doc(). Got rid of get_cmd_here_doc(). Added 42 header

--- pipex/bonus/here_doc_bonus.c
+++ pipex/bonus/here_doc_bonus.c
@@ -1,34 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   here_doc_bonus.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbechtol <fbechtol@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 14:49:44 by fbechtol          #+#    #+#             */
+/*   Updated: 2022/01/26 14:49:45 by fbechtol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "pipex_bonus.h"
 
 static char	*join_here_doc(t_pipex *data, char *tmp_next_line, char *limiter);
-
-void	get_cmd_here_doc(int argc, char **argv, t_pipex *data)
-{
-	t_cmd_node	*new_node;
-	int			i;
-	char		**tmp_argv;
-
-	i = argc - 2;
-	while (i >= 3)
-	{
-		new_node = (t_cmd_node *)malloc(sizeof(t_cmd_node));
-		if (!new_node)
-			err_exit(data, "Error: malloc new_node", 22);
-		data->cmds = new_node;
-		data->cmds->tmp_argv_cmds = ft_strdup(argv[i]);
-		if (!data->cmds->tmp_argv_cmds)
-			err_exit(data, "Error: strdup(argv[1]", 21);
-		tmp_argv = ft_split(argv[i], ' ');
-		if (!tmp_argv)
-			err_exit(data, "Error: split argv[i]", 20);
-		data->cmds->cmd_path = ft_strdup(tmp_argv[0]);
-		if (!data->cmds->cmd_path)
-			err_exit(data, "Error: strdup(tmp_argv[0]", 25);
-		free_two_d_arr(tmp_argv);
-		add_node(data, new_node);
-		i--;
-	}
-}
 
 void	read_from_here_doc(t_pipex *data, char **argv)
 {
@@ -40,7 +24,7 @@ void	read_from_here_doc(t_pipex *data, char **argv)
 		err_exit(data, "Error: ft_strjoin(limiter)", 26);
 	tmp_next_line = get_next_line(0);
 	if (ft_strncmp(tmp_next_line, limiter, ft_strlen(tmp_next_line) != 0))
-		data->here_doc = ft_strdup(data->here_doc);
+		data->here_doc = ft_strdup(tmp_next_line);
 	while (ft_strncmp(tmp_next_line, limiter, ft_strlen(tmp_next_line)) != 0)
 		tmp_next_line = join_here_doc(data, tmp_next_line, limiter);
 	free(limiter);
@@ -48,7 +32,6 @@ void	read_from_here_doc(t_pipex *data, char **argv)
 	free(tmp_next_line);
 	tmp_next_line = NULL;
 }
-
 
 static char	*join_here_doc(t_pipex *data, char *tmp_next_line, char *limiter)
 {
