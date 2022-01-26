e07be5c0897206ffd5344038b191b038aa7098e5
Fredi-B <fredibechtoldt@outlook.com>
2022-01-26 15:57:18 +0100

    added prep_new_node() prototype

--- pipex/bonus/pipex_bonus.h
+++ pipex/bonus/pipex_bonus.h
@@ -6,7 +6,7 @@
 /*   By: fbechtol <fbechtol@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 15:11:58 by fbechtol          #+#    #+#             */
-/*   Updated: 2022/01/25 17:53:16 by fbechtol         ###   ########.fr       */
+/*   Updated: 2022/01/26 12:18:28 by fbechtol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,8 @@
 
 /* ---------------------- Defines for debugging ---------------------------- */
 
-# define dsprintf(expr) printf("\n" #expr ":\n|%s|\n", expr)
-# define diprintf(expr) printf("\n" #expr ":\n|%d|\n", expr)
+/* # define dsprintf(expr) printf("\n" #expr ":\n|%s|\n", expr)
+# define diprintf(expr) printf("\n" #expr ":\n|%d|\n", expr) */
 
 /* ----------------------------- Structures -------------------------------- */
 
@@ -66,37 +66,36 @@ typedef struct s_pipex
 
 /*  -------------------------- parsing_bonus.c ----------------------------- */
 
-void	parsing(int argc, char **argv, char **env, t_pipex *data);
+void		parsing(int argc, char **argv, char **env, t_pipex *data);
 
 /*  ----------------------- parsing_utils_bonus.c -------------------------- */
 
-char	*add_slash_and_join(char *path, char *cmd_part);
-void	add_node(t_pipex *data, t_cmd_node *new_node);
-void	store_cmds(t_pipex *data, char *tmp_c_p, int i);
-int		get_prefix_len(char *env);
+char		*add_slash_and_join(char *path, char *cmd_part);
+t_cmd_node	*prep_new_node(t_pipex *data, char **argv, int i);
+void		add_node(t_pipex *data, t_cmd_node *new_node);
+void		store_cmds(t_pipex *data, char *tmp_c_p, int i);
+int			get_prefix_len(char *env);
 
 /*  --------------------------- piping_bonus.c ----------------------------- */
 
-void	piping(t_pipex *data);
-void	read_write_pipe(t_pipex *data, int *pipe_r, int *pipe_w);
+void		piping(t_pipex *data);
+void		read_write_pipe(t_pipex *data, int *pipe_r, int *pipe_w);
 
 /*  ------------------------ piping_utils_bonus.c -------------------------- */
 
-int		fork_write_child(t_pipex *data);
-int		check_for_middle_cmds(t_pipex *data);
-void	check_pipe_flag(t_pipex *data);
+int			fork_write_child(t_pipex *data);
+int			check_for_middle_cmds(t_pipex *data);
+void		check_pipe_flag(t_pipex *data);
 
 /*  ------------------------ here_doc_bonus.c ------------------------- */
 
-void	get_cmd_here_doc(int argc, char **argv, t_pipex *data);
-void	read_from_here_doc(t_pipex *data, char **argv);
-void	put_here_doc_in_pipe(t_pipex *data);
-
+void		read_from_here_doc(t_pipex *data, char **argv);
+void		put_here_doc_in_pipe(t_pipex *data);
 
 /*  --------------------------- err_exit_bonus.c --------------------------- */
 
-void	free_data(t_pipex *data);
-void	err_exit(t_pipex *data, char *msg, int len);
-void	free_two_d_arr(char **arr);
+void		free_data(t_pipex *data);
+void		err_exit(t_pipex *data, char *msg, int len);
+void		free_two_d_arr(char **arr);
 
 #endif
