fb207d5cc9c0202f82925cc4b2010284826f9c52
Fredi-B <fredibechtoldt@outlook.com>
2022-01-26 15:54:41 +0100

    added error handling for wrong input with here_doc. Added here_doc_flag in get_cmds(), needed to write prep_new_node for the norm

--- pipex/bonus/parsing_bonus.c
+++ pipex/bonus/parsing_bonus.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing_bonus.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbechtol <fbechtol@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 14:49:50 by fbechtol          #+#    #+#             */
+/*   Updated: 2022/01/26 15:51:34 by fbechtol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "pipex_bonus.h"
 
 static char	*get_paths_from_env(char **env, t_pipex *data);
@@ -7,11 +19,13 @@ static void	get_files(int argc, char **argv, t_pipex *data);
 
 void	parsing(int argc, char **argv, char **env, t_pipex *data)
 {
-	get_paths_from_env(env, data);
 	if (data->here_doc_flag == YES)
-		get_cmd_here_doc(argc, argv, data);
-	else
-		get_cmds(argc, argv, data);
+	{
+		if (argc < 6)
+			err_exit(data, "Use: here_doc LIMITER \"cmd1\" \"cmd2\" <file>", 42);
+	}
+	get_paths_from_env(env, data);
+	get_cmds(argc, argv, data);
 	join_path_and_cmds(data);
 	get_files(argc, argv, data);
 }
@@ -41,25 +55,16 @@ static void	get_cmds(int argc, char **argv, t_pipex *data)
 {
 	t_cmd_node	*new_node;
 	int			i;
-	char		**tmp_argv;
+	int			no_cmd_args;
 
 	i = argc - 2;
-	while (i >= 2)
+	if (data->here_doc_flag == YES)
+		no_cmd_args = 3;
+	else
+		no_cmd_args = 2;
+	while (i >= no_cmd_args)
 	{
-		new_node = (t_cmd_node *)malloc(sizeof(t_cmd_node));
-		if (!new_node)
-			err_exit(data, "Error: malloc new_node", 22);
-		data->cmds = new_node;
-		data->cmds->tmp_argv_cmds = ft_strdup(argv[i]);
-		if (!data->cmds->tmp_argv_cmds)
-			err_exit(data, "Error: strdup(argv[1]", 21);
-		tmp_argv = ft_split(argv[i], ' ');
-		if (!tmp_argv)
-			err_exit(data, "Error: split argv[i]", 20);
-		data->cmds->cmd_path = ft_strdup(tmp_argv[0]);
-		if (!data->cmds->cmd_path)
-			err_exit(data, "Error: strdup(tmp_argv[0]", 25);
-		free_two_d_arr(tmp_argv);
+		new_node = prep_new_node(data, argv, i);
 		add_node(data, new_node);
 		i--;
 	}
