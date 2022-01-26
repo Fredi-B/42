4ce2c04ab1e3fd23020045ce4aca7cc400e46f45
Fredi-B <fredibechtoldt@outlook.com>
2022-01-26 15:55:48 +0100

    wrote prep_new_node(). added 42 header

--- pipex/bonus/parsing_utils_bonus.c
+++ pipex/bonus/parsing_utils_bonus.c
@@ -1,7 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing_utils_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbechtol <fbechtol@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 14:49:56 by fbechtol          #+#    #+#             */
+/*   Updated: 2022/01/26 14:50:15 by fbechtol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "pipex_bonus.h"
 
-//für mandatory part total unnötig:
-//Länge vom prefix hat man ja schon im if statement
 int	get_prefix_len(char *env)
 {
 	char	**prefix;
@@ -19,6 +29,28 @@ int	get_prefix_len(char *env)
 	}
 	free(prefix);
 	return (len + 1);
+}
+
+t_cmd_node	*prep_new_node(t_pipex *data, char **argv, int i)
+{
+	t_cmd_node	*new_node;
+	char		**tmp_argv;
+
+	new_node = (t_cmd_node *)malloc(sizeof(t_cmd_node));
+	if (!new_node)
+		err_exit(data, "Error: malloc new_node", 22);
+	data->cmds = new_node;
+	data->cmds->tmp_argv_cmds = ft_strdup(argv[i]);
+	if (!data->cmds->tmp_argv_cmds)
+		err_exit(data, "Error: strdup(argv[1]", 21);
+	tmp_argv = ft_split(argv[i], ' ');
+	if (!tmp_argv)
+		err_exit(data, "Error: split argv[i]", 20);
+	data->cmds->cmd_path = ft_strdup(tmp_argv[0]);
+	if (!data->cmds->cmd_path)
+		err_exit(data, "Error: strdup(tmp_argv[0]", 25);
+	free_two_d_arr(tmp_argv);
+	return (new_node);
 }
 
 void	add_node(t_pipex *data, t_cmd_node *new_node)
